f307e365c54272007e429a85636c3109ec6a9a3f
Diego Cornejo <diegof.cornejo@gmail.com>
2024-11-04 12:09:52 -0600

    chore: Add responsive banner size

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -11,27 +11,27 @@ import 'swiper/css/autoplay';
 const slides = [
 	{
 		id: 1,
-		image: 'https://placehold.co/600x400/000000/FFFFFF/png',
+		image: 'https://placehold.co/1200x400/999999/FFFFFF/png',
 	},
 	{
 		id: 2,
-		image: 'https://placehold.co/600x400/FF0000/FFFFFF/png',
+		image: 'https://placehold.co/1200x400/8A9BA8/FFFFFF/png',
 	},
 	{
 		id: 3,
-		image: 'https://placehold.co/600x400/00FF00/FFFFFF/png',
+		image: 'https://placehold.co/1200x400/9A8CA8/FFFFFF/png',
 	},
 	{
 		id: 4,
-		image: 'https://placehold.co/600x400/0000FF/FFFFFF/png',
+		image: 'https://placehold.co/1200x400/A88A8A/FFFFFF/png',
 	},
 	{
 		id: 5,
-		image: 'https://placehold.co/600x400/FF00FF/FFFFFF/png',
+		image: 'https://placehold.co/1200x400/A8A58A/FFFFFF/png',
 	},
 	{
 		id: 6,
-		image: 'https://placehold.co/600x400/FFFF00/FFFFFF/png',
+		image: 'https://placehold.co/1200x400/7F8A8A/FFFFFF/png',
 	}
 ];
 
@@ -59,7 +59,7 @@ export default () => {
 				{slides.map((slide) => (
 					<SwiperSlide key={slide.id}>
 						<div className='h-auto w-full bg-base-200 flex justify-center items-center'>
-							<img src={slide.image} alt={`Slide ${slide.id}`} width={600} height={400} onClick={handleToast} className='cursor-pointer'/>
+							<img src={slide.image} alt={`Slide ${slide.id}`} onClick={handleToast} className='cursor-pointer w-full aspect-[3/1] md:aspect-[4/1] object-cover'/>
 						</div>
 					</SwiperSlide>
 				))}
