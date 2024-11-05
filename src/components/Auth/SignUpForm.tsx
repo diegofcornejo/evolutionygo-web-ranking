6cb38083f8de6ce4b219d9b9682c960f6e182699
Diego Cornejo <diegof.cornejo@gmail.com>
2024-11-05 08:28:04 -0600

    feat: Load SVG from file

--- src/components/Auth/SignUpForm.tsx
+++ src/components/Auth/SignUpForm.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import { toast } from 'sonner';
+import { ReactSVG } from 'react-svg'
 
 export default function LoginForm({ dialog }: { dialog: string }) {
 	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [username, setUsername] = useState('');
 	const [error, setError] = useState<string | null>(null);
 
 
 	const cleanForm = () => {
 		setEmail('');
-		setPassword('');
+		setUsername('');
 		setError(null);
 	}
 
@@ -23,8 +24,8 @@ export default function LoginForm({ dialog }: { dialog: string }) {
 
 	const handleSubmit = async (event: { preventDefault: () => void; }) => {
 		event.preventDefault();
-		console.log(email, password);
-		if (email === 'admin@admin.com' && password === 'admin') {
+		console.log(email, username);
+		if (email === 'admin@admin.com' && username === 'admin') {
 			toast.success('Login successful', {
 				position: 'bottom-center',
 			});
@@ -60,14 +61,7 @@ export default function LoginForm({ dialog }: { dialog: string }) {
 	return (
 		<form className='space-y-4' onSubmit={handleSubmit}>
 			<label className="input input-bordered flex items-center gap-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 16 16"
-					fill="currentColor"
-					className="h-4 w-4 opacity-70">
-					<path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-					<path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-				</svg>
+				<ReactSVG src='/icons/email.svg' className='w-4 h-4 opacity-70'/>
 				<input
 					type="text"
 					className="grow"
@@ -79,24 +73,13 @@ export default function LoginForm({ dialog }: { dialog: string }) {
 				/>
 			</label>
 			<label className="input input-bordered flex items-center gap-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 16 16"
-					fill="currentColor"
-					className="h-4 w-4 opacity-70">
-					<path
-						fillRule="evenodd"
-						d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-						clipRule="evenodd"
-					/>
-				</svg>
+				<ReactSVG src='/icons/username.svg' className='w-4 h-4 opacity-70'/>
 				<input
-					type="password"
+					type="text"
 					className="grow"
-					name="password"
-					placeholder="Password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
 					required
 				/>
 			</label>
