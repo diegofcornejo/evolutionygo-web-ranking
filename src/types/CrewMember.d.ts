024aa52c1dd7e186b259e70c58439ce59df3d158
Diego Cornejo <diegof.cornejo@gmail.com>
2024-11-18 23:08:24 -0600

    feat: Add crew links

--- src/types/CrewMember.d.ts
+++ src/types/CrewMember.d.ts
@@ -1,6 +1,11 @@
+type Link = {
+	icon: string;
+	url: string;
+};
+
 export type CrewMember = {
 	name: string;
 	role: string;
 	picture: string;
-	links: { [key: string]: string };
+	links?: Link[];
 };
