191cb3ab59f764a52ff708d77d5ae86225a98f2f
Diego Cornejo <diegof.cornejo@gmail.com>
2024-11-19 12:01:39 -0600

    style: Update forms title size

--- src/components/Settings/SettingsForm.tsx
+++ src/components/Settings/SettingsForm.tsx
@@ -70,75 +70,78 @@ export default function SettingsForm({ dialog }: { dialog: string }) {
 
 	return (
 		<div>
-			<form className="space-y-2 mt-8"
-				onSubmit={handleSaveUsername}>
-				<h2 className="font-bold">Change Username</h2>
-				<label className="input input-bordered flex items-center gap-2">
-					<ReactSVG src="/icons/username.svg" className="w-4 h-4 opacity-70" />
-					<input
-						type="text"
-						className="grow"
-						placeholder="New Username"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
-				</label>
-				<p
-					className={`text-error text-sm h-4 transition-opacity duration-300 ${!usernameError ? 'opacity-0' : 'opacity-100'
-						}`}
-				>
-					{usernameError || ' '}
-				</p>
-				<button
-					className="btn btn-primary btn-sm"
-					type="submit"
-				>
-					Save Username
-				</button>
-			</form>
-			<div className="divider"></div>
-			<form className="space-y-2 mt-4" onSubmit={handleSavePassword}>
-				<h2 className="font-bold">Change Password</h2>
-				<label className="input input-bordered flex items-center gap-2">
-					<ReactSVG src="/icons/password.svg" className="w-4 h-4 opacity-70" />
-					<input
-						type="password"
-						className="grow"
-						placeholder="Actual Password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</label>
-				< label className="input input-bordered flex items-center gap-2" >
-					<ReactSVG src="/icons/password.svg" className="w-4 h-4 opacity-70" />
-					<input
-						type="password"
-						className="grow"
-						placeholder="New Password"
-						value={newPassword}
-						maxLength={4}
-						onChange={(e) => setNewPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<p
-					className={`text-error text-sm h-4 transition-opacity duration-300 ${!passwordError ? 'opacity-0' : 'opacity-100'
-						}`}
-				>
-					{passwordError || ' '}
-				</p>
-				<button
-					className="btn btn-primary btn-sm"
-					type="submit"
-				>
-					Save Password
-				</button>
-			</form>
-			<div className='flex justify-end gap-2'>
-				<button className="btn btn-outline" type="button" onClick={closeModal}>Close</button>
+			<div>
+				<div className="divider">Change Username</div>
+				<form className="space-y-2 mt-8"
+					onSubmit={handleSaveUsername}>
+					<label className="input input-bordered flex items-center gap-2">
+						<ReactSVG src="/icons/username.svg" className="w-4 h-4 opacity-70" />
+						<input
+							type="text"
+							className="grow"
+							placeholder="New Username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
+						/>
+					</label>
+					<p
+						className={`text-error text-xs h-4 transition-opacity duration-300 ${!usernameError ? 'opacity-0' : 'opacity-100'
+							}`}
+					>
+						{usernameError || ' '}
+					</p>
+					<button
+						className="btn btn-primary btn-sm"
+						type="submit"
+					>
+						Save
+					</button>
+				</form>
 			</div>
+			<div className='mt-12'>
+				<div className="divider">Change Password</div>
+				<form className="space-y-2 mt-8" onSubmit={handleSavePassword}>
+					<label className="input input-bordered flex items-center gap-2">
+						<ReactSVG src="/icons/password.svg" className="w-4 h-4 opacity-70" />
+						<input
+							type="password"
+							className="grow"
+							placeholder="Actual Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</label>
+					< label className="input input-bordered flex items-center gap-2" >
+						<ReactSVG src="/icons/password.svg" className="w-4 h-4 opacity-70" />
+						<input
+							type="password"
+							className="grow"
+							placeholder="New Password"
+							value={newPassword}
+							maxLength={4}
+							onChange={(e) => setNewPassword(e.target.value)}
+							required
+						/>
+					</label>
+					<p
+						className={`text-error text-xs h-4 transition-opacity duration-300 ${!passwordError ? 'opacity-0' : 'opacity-100'
+							}`}
+					>
+						{passwordError || ' '}
+					</p>
+					<button
+						className="btn btn-primary btn-sm"
+						type="submit"
+					>
+						Save
+					</button>
+				</form>
+			</div>
+			{/* <div className='flex justify-end gap-2'>
+				<button className="btn btn-outline" type="button" onClick={closeModal}>Close</button>
+			</div> */}
 		</div>
 	);
 }
