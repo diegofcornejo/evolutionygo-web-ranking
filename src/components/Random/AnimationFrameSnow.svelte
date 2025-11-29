f312c318a6016d2651ad5200f58330314d27cd8b
Diego Cornejo <diegof.cornejo@gmail.com>
2025-11-29 17:59:27 -0600

    feat: update AnimationFrameSnow component positioning and integrate into layout

--- src/components/Random/AnimationFrameSnow.svelte
+++ src/components/Random/AnimationFrameSnow.svelte
@@ -91,12 +91,13 @@
 
   .snowframe {
     pointer-events: none;
-    position: absolute;
+    position: fixed;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
     overflow: hidden;
+    z-index: 1000;
   }
 </style>
 
